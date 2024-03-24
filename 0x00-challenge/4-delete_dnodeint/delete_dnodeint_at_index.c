865676c7ae49a7e68926261f6b5f4200e177b985
xola-mthembu <xola.mthembu@gmail.com>
2024-03-24 13:09:24 -0700

    4-delete_dnodeint/ updated

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *current;
 	unsigned int i;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	current = *head;
